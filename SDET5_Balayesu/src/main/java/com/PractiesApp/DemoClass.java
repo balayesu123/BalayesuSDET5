4d70a48689db34aae0d6f42bf7c6348e3dc3df31
Padmaja A <Padmaja A@LAPTOP-OVBCR4D3>
2022-04-13 18:02:31 +0530

    padma

--- SDET5_Balayesu/src/main/java/com/PractiesApp/DemoClass.java
+++ SDET5_Balayesu/src/main/java/com/PractiesApp/DemoClass.java
@@ -1,5 +1,25 @@
 package com.PractiesApp;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+/**
+ * 
+ * @author Padmaja A
+ *
+ */
 public class DemoClass {
+	
+	public void Demo() {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("www.google.com");
+		driver.findElement(By.id("input")).click();
+		}
 
 }
